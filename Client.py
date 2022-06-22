32a418b871cc07dd94e6a2bfee4279d7ee991dc6
Juano <vazquezjc097@gmail.com>
2022-06-22 05:01:01 -0400

    working (apparently) chord

--- Client.py
+++ Client.py
@@ -1,30 +1,81 @@
-import struct
-from xmlrpc.client import Boolean
+from hashlib import sha512
+import threading
 from Agent import *
-import argparse
+from tools import *
+import socket
+import struct
+import sys
 
+class Node:
+    def __init__(self, ip, port):
+        self.ip = ip
+        self.port = port
+        self.address = (ip,port)
+
+        self.id = getHash(ip+port)
+        try:
+            self.ServerSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+            self.ServerSocket.bind((IP, PORT))
+            self.ServerSocket.listen()
+        except socket.error:
+            print("Socket error")
+        print(self.id)
 
 
 def main():
-    agents = []
-    categories = []
-    while(True):
-        print("Ingrese Agente")
-        addAgent(agents)
-        if len(agents) == 3:
-            break
-    for agent in agents:
-        print(agent.id)
-    
+    print(sys.argv)
+    node = Node(sys.argv[1],sys.argv[2])
+    startClient(node)
 
-def addAgent(agents = list):
+def startClient(node):
+    threading.Thread(target=listenThread, args=(node)).start()
+    threading.Thread(target=pingSucc, args=(node)).start()
+    conectClient(node)
+    while True:
+        print('Listening')
+        depMenu(node)
+
+def listenThread(node):
+    while True:
+        try:
+            connection, address = node.ServerSocket.accept()
+            connection.settimeout(120)
+            threading.Thread(target= connectionThread, args=(node, connection, address)).start()                
+        except socket.error:
+            pass
+
+def pingSucc(node):
+    pass
+
+def connectionThread(node, connection, address):
+    pass
+
+def conectClient(node):
+    ip = input('Enter IP to connect: ')
+    port = input('Enter port: ')
+    name = input('Enter user name: ')
+    return node
+
+def depMenu(node):
+    user_input = input()
+    if user_input == '1':
+        objective = input()
+        agent = searchAgent(node,objective)
+        try:
+            print("El producto {0} se encuentra en existencia".format(agent.name))
+        except:
+            print("El producto {0} no se encuentra en existencia".format(objective))
+    elif user_input == '2':
+        pass   
+
+def addAgent():
     inp = input().split(" ")
     data = struct
     data.address = (inp[0], inp[1])
     data.id = inp[2]
     data.admin = False
-    agent = Agent(data)
-    agents.append(agent)
+    return Agent(data)
+    
 
 def updAgentStatus(agent = Agent, status = bool):
     agent.admin = status
--- tools.py
+++ tools.py
@@ -4,7 +4,7 @@ import hashlib
 IP = '127.0.0.1'
 PORT = 8080
 BUFFER = 4096
-MAX_BITS = 10
+MAX_BITS = 20
 MAX_NODES = 2 ** MAX_BITS
 
 def getHash(key):
@@ -13,4 +13,4 @@ def getHash(key):
     a 10-bit (1024) compressed integer.
     '''
     result = hashlib.sha1(key.encode())
-    return int(result.hexdigest(), 16) % MAX_NODES
+    return int(result.hexdigest(), 16) % 1000
